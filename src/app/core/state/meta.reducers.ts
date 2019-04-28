59b933166870d7858c1193be9a59a25566c13e9b
Kristian Poslek <kristian@poslek.com>
2019-04-28 21:35:57 +0200

    production build fix

--- src/app/core/state/meta.reducers.ts
+++ src/app/core/state/meta.reducers.ts
@@ -1,21 +1,24 @@
 import { ActionReducer, MetaReducer, State } from '@ngrx/store';
+import { environment } from 'src/environments/environment';
 
 function logger(reducer: ActionReducer<State<any>>): ActionReducer<State<any>> {
-  return (state: State<any>, action: any): State<any> => {
-    const now = new Date();
-    console.group('State change:', action.type);
-    console.log(
-      `Time: ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${now.getMilliseconds()}`
-    );
-    console.log('Old state:', state);
-    console.log('Payload:', action.payload);
+    return (state: State<any>, action: any): State<any> => {
+        const now = new Date();
+        console.group('State change:', action.type);
+        console.log(
+            `Time: ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${now.getMilliseconds()}`
+        );
+        console.log('Old state:', state);
+        console.log('Payload:', action.payload);
 
-    const newState = reducer(state, action);
-    console.log('New state:', newState);
-    console.groupEnd();
+        const newState = reducer(state, action);
+        console.log('New state:', newState);
+        console.groupEnd();
 
-    return newState;
-  };
+        return newState;
+    };
 }
 
-export const metaReducers: MetaReducer<any>[] = [logger];
+export const metaReducers: MetaReducer<any>[] = environment.production
+    ? []
+    : [logger];
